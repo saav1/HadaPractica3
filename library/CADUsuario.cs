33cb3482aea1fcfa0f97bf5c02e0b79c61adc647
Stalyn <saav1>
2019-03-29 14:45:35 +0100

    CAD y EN

--- library/CADUsuario.cs
+++ library/CADUsuario.cs
@@ -8,5 +8,38 @@ namespace library
 {
     class CADUsuario
     {
+        //Inicializa la cadena de conexión de la DB.
+        public CADUsuario() { }
+
+        //Crea un nuevo usuario en la BD con los datos del usuario
+        public bool createUsuario(ENUsuario en) {
+            return true;
+        }
+
+        //Devuelve solo el usuario indicado leído de la DB
+        public bool readFirstUsuario(ENUsuario en) {
+            return true;
+        }
+
+        //Devuelve solo el usuario siguiente al indicado
+        public bool readNextUsuario(ENUsuario en) {
+            return true;
+        }
+
+        //Devuelve solo el usuario anterior al indicado
+        public bool readPrevUsuario(ENUsuario en) {
+            return true;
+        }
+
+        //Actualiza los datos de un usuario en la BD con los datos del
+        //usuario por el parámetro en.
+        public bool updateUsuario(ENUsuario en) {
+            return true;
+        }
+
+        //Borra el usuario representado en 'en' de la BD
+        public bool deteleUsuario(ENUsuario en) {
+            return true;
+        }
     }
 }
--- library/ENUsuario.cs
+++ library/ENUsuario.cs
@@ -8,5 +8,51 @@ namespace library
 {
     public class ENUsuario
     {
+        public ENUsuario() { }
+
+        public ENUsuario(string nif, string nombre, int edad) { }
+
+        /*Guarda este usuario en la BD. Para ello hará uso de los métodos 
+        apropiados de CADUsuario. Devuelve 'false' si no se ha podido realizar
+        la operación.*/
+        public bool createUsuario() {
+            return true;
+        }
+
+        /*Recupera todos los usuarios de la BD y devuelve solo el primer usuario.
+         Para ello hará uso de los métodos apropiados de CADUsuario. Devuelve 'false'
+         si no se ha podido realizar la operación.*/
+        public bool readFirstUsuario() {
+            return true;
+        }
+
+        /*Recupera todos los usuarios de la BD y devuelve solo el susuario siguiente al indicado.
+         Para ello hará uso de los métodos apropiados de CADUsuario. Devuelve 'false' si no se 
+         ha podido realizar la operación.*/
+        public bool readNextUsuario() {
+            return true;
+        }
+
+        /*Recupera todos los usuarios de la BD y devuelve solo el usuario anterior al indicado.
+         Para ello hará uso de los métodos apropiados de CADUsuario. Devuelve 'false' si no se
+         ha podido realizar la operación.*/
+        public bool readPrevUsuario() {
+            return true;
+        }
+
+        /*Actualiza este usuario en la BD. Para ello hará uso de los métodos apropiados de CADUsuario.
+         Devuelve 'false' si no se ha podido realizar la operación.*/
+        public bool updateUsuario() {
+            return true;
+        }
+
+        /*Borra este usuario de la BD. Para ello hará uso de lo métodos apropiados de CADUsuario. 
+         Devuelve 'false' si no se ha podido realizar la operación.*/
+        public bool deleteUsuario() {
+            return true;
+        }
+
+
+
     }
 }
